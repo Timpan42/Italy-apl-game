4eb5413776ff2104a128a794ca9e1a43a8d6c4d7
timpan42 <tim@fagerdal.se>
2024-05-03 15:57:35 +0200

    buttons to change sliders, fixed debugs

--- Scripts/UI/Options-Ui.gd
+++ Scripts/UI/Options-Ui.gd
@@ -3,6 +3,7 @@ extends Control
 @export var masterSlider : OptionSlider
 @export var musicSlider : OptionSlider
  
+signal closeOptions
 
 func _ready():
 	masterSlider._on_start(AudioManager.masterVolume)
@@ -14,8 +15,8 @@ func _save_audio_seting():
 	AudioManager._save_to_file()
 
 func _on_back_pressed():
-	hide()
 	_save_audio_seting()
+	closeOptions.emit()
 
 func _on_save_pressed():
 	_save_audio_seting()
--- Scripts/UI/Ui-Manager.gd
+++ Scripts/UI/Ui-Manager.gd
@@ -3,7 +3,14 @@ extends Control
 @export var pauseUi : Control
 @export var optionUi : Control
 
+func _ready():
+	#pauseUi.hide()
+	optionUi.hide()
 
 func _on_pause_ui_open_options():
-	pauseUi.visible = false
-	optionUi.visible = true
+	pauseUi.hide()
+	optionUi.show()
+
+func _on_options_ui_close_options():
+	pauseUi.show()
+	optionUi.hide()
